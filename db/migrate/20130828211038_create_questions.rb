384f5792149f8cea823753219f2f57e7456c578f
Micah Sherman <micahsherman@mac.com>
2013-08-28 17:10:44 -0500

    finished models and migrations

--- db/migrate/20130828211038_create_questions.rb
+++ db/migrate/20130828211038_create_questions.rb
@@ -1,6 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.belongs_to :user
       t.string :title
       t.text :body
       t.integer :rating, default: 0
