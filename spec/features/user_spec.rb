be14e1eb5414e692052c78b9cf61c1b4a3341b1f
Abi Noda <a@abinoda.com>
2013-08-28 16:34:14 -0500

    still testing user login

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,13 +1,25 @@
 require_relative '../spec_helper'
 
 feature "User login" do
+  
   context "on sign in page" do
-    it "encounters the required text boxes" do
+    
+    # it "encounters the required text boxes" do
+    #   visit '/users/new'
+    #   page.has_field?('Name', type: :name)
+    #   page.has_field?('Email', type: :email)
+    #   page.has_field?('Password', type: :password)
+    #   page.has_field?('Confirm Password', type: :password_confirmation)
+    # end
+
+    it "should sign in correctly" do
       visit '/users/new'
-      page.has_field?('Name', type: :name)
-      page.has_field?('Email', type: :email)
-      page.has_field?('Password', type: :password)
-      page.has_field?('Confirm Password', type: :password_confirmation)
+      save_and_open_page
+      fill_in 'Name', :with => 'jer'
+      fill_in 'Email', :with => 'j@l.com'
+      fill_in 'Password', :with => '5monkeys'
+      fill_in 'Confirm Password', :with => '5monkeys'
+      click_button 'Create'
     end
   end
 end
