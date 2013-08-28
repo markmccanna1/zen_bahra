33770c8421f802034d915d93e9332ee7cf87ce3b
Micah Sherman <micahsherman@mac.com>
2013-08-28 16:17:27 -0500

    generated question model and got tests failing

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,19 +1,43 @@
 require 'spec_helper'
 
 describe 'Question' do
+ 
   before(:each) do
     @question = Question.new(:title => "first question", :body => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
   end
 
-  it "should have a title" do
-    @question.title.should eq ("first question")
+  describe 'created_at' do
+    context "when a question is initialized" do
+      it "created_at should be set" do
+        Time.stub(:new).and_return("time")
+        @question = Question.new("a", "b")
+        @question.created_at.should eq "time"
+      end
+    end
   end
 
-  it "should have a body" do
-    @question.body.should eq ("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+  describe 'title' do
+    context "when a question is initialized" do
+      it "should have a title" do
+        expect(@question.title).to eq "first question"
+      end
+    end
   end
 
-  it "Should have a rating with a default value of 0" do
-    @question.rating.should eq (0)
+  describe 'title' do
+    context "when a question is initialized" do
+      it "should have a body" do
+        expect(@question.body).to eq "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      end
+    end
   end
+
+  describe 'rating default' do
+    context "when a question is initialized" do 
+      it "Should have a rating with a default value of 0" do
+        expect(@question.rating).to eq 0
+      end
+    end
+  end
+
 end
