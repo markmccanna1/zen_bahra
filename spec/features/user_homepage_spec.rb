70ca167cbd91fb2b1517e9ac6c5dd11160d4fbf5
Micah Sherman <micahsherman@mac.com>
2013-08-29 11:23:19 -0500

    you can view all of a users tags on their profile

---
+++ spec/features/user_homepage_spec.rb
@@ -0,0 +1,11 @@
+# require 'database_cleaner'
+# require 'spec_helper'
+# DatabaseCleaner.strategy = :truncation
+
+# feature "User homepage" do
+  
+#   context "on user homepage" do
+
+#   end
+
+# end
