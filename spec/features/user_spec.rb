239ed2433d5bfb6c5bd08a3f7b8f090c4cc3ffbe
Jerry VanStrydonck <jvanstry@gmail.com>
2013-08-29 14:34:25 -0500

    integration testing for user login

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,19 +2,39 @@ require 'database_cleaner'
 require_relative '../spec_helper'
 DatabaseCleaner.strategy = :truncation
 
-feature "User login" do
+feature "User gets to signup page" do
   
-  context "on sign in page" do
+  before(:each) do
+    visit new_user_path
+  end
 
-    it "should sign in correctly", :js => true do
-      visit(new_user_path)
-      save_and_open_page
-      fill_in 'user_name', :with => 'jer'
-      fill_in 'user_email', :with => 'j@l.com'
-      fill_in 'user_password', :with => '5monkeys'
-      fill_in 'user_password_confirmation', :with => '5monkeys'
-      click_button('create_user')
-    end
+  scenario "sign up form present", :js => true do
+    expect(page).to have_css('form')
+  end
+
+  scenario "user able to sign up", :js => true do
+    fill_in 'user_name', :with => 'jer'
+    fill_in 'user_email', :with => 'j@l.com'
+    fill_in 'user_password', :with => '5monkeys'
+    fill_in 'user_password_confirmation', :with => '5monkeys'
+    click_button 'create_user'
+  end
+end
+
+feature "User signin" do
+
+  before(:each) do
+    visit questions_path 
+  end
+
+  scenario "sign in form is present", :js => true do
+    expect(page).to have_css('#login_form')
+  end
+    
+  scenario "user can sign in" do   
+    fill_in 'email', :with => 'j@l.com'
+    fill_in 'password', :with => '5monkeys'
+    click_button 'login'
   end
 end
 
