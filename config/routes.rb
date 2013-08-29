f0c0c49de8a694a868ab759e3696d4e7a14e2944
Jerry VanStrydonck <jvanstry@gmail.com>
2013-08-29 14:34:41 -0500

    user login implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 MyApp::Application.routes.draw do
-
   root :to => "home#index"
 
   resources :users
 
   resources :questions
+
   resources :comments
+
+  post '/login', :to => 'sessions#create', as: 'login'
+  delete '/logout', :to => 'sessions#destroy', :via => :delete
 end
