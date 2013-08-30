305b92b5ed448bf55a967ee9619974985551677b
Abi Noda <a@abinoda.com>
2013-08-29 20:04:07 -0500

    added comment relationship to commentables

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,5 @@ class Answer < ActiveRecord::Base
 
   belongs_to :question
   belongs_to :user
-  
+  has_many :comments, :as => :commentable
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :commentable, polymorphic: true
+  belongs_to :user
 
   validates :body, presence: true
   attr_accessible :body, :user_id, :commentable_id
